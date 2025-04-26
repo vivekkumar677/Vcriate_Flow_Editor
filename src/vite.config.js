9df3b9d89ebf9168959b4b3d51fbc9f77f3a9067
Vivek Kumar <ervivekkumar677@gmail.com>
2025-04-26 21:27:52 +0530

    Vite file added

---
+++ src/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+  base: process.env.VITE_BASE_PATH ||  "/react-vite-deploy",
+});
